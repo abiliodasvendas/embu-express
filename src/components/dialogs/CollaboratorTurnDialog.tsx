289abc92a65f7aae7c948393679ed5f106225304
Thiago Barros <ext.thiago.abilio@mills.com.br>
2026-05-29 12:52:04 -0300

    wip

--- src/components/dialogs/CollaboratorTurnDialog.tsx
+++ src/components/dialogs/CollaboratorTurnDialog.tsx
@@ -18,13 +18,6 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
-import {
   useCollaborator,
   useCreateVinculo,
   useEmpresas,
